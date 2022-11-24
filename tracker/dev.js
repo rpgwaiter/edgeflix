7ea549209f8848ab53722196e3c61e12000385bc
rpgwaiter <rpgwaiter@based.zone>
2022-11-24 01:36:56 -0700

    remove old vars from dev.js

--- tracker/dev.js
+++ tracker/dev.js
@@ -1,5 +1,4 @@
 import { Miniflare } from 'miniflare'
-import { CrossKV } from 'cross-cf'
 import fetch from 'cross-fetch'
 import * as dotenv from 'dotenv'
 
@@ -7,15 +6,7 @@ dotenv.config({ path: '../.env' })
 
 globalThis.fetch = fetch
 
-const { PORT = 8787, EDGEFLIX_KV_ID } = process.env
-
-const EDGEFLIX_KV = EDGEFLIX_KV_ID
-  ? new CrossKV('EDGEFLIX-KV-TESTING', { target: 'remote', kvID: EDGEFLIX_KV_ID })
-  : undefined
-
-if (!EDGEFLIX_KV) {
-  throw new Error('KV is not setup properly. Be sure your Cloudflare credentials are in your env as mentioned in the readme.')
-}
+const { PORT = 8787 } = process.env
 
 const mf = new Miniflare({
   name: 'edgeflix-tracker',
